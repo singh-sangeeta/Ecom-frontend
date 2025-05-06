c9bfd978f8d9622b0c13644354f0277f6066d1fe
sangeeta-gehlot <sangeeta.gehlot@velocis.co.in>
2025-05-06 16:40:09 +0530

    initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <label htmlFor="username">UserName</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+            </form>
+        </div>
+    )
+}
+export default Login
